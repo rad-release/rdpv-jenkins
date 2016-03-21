f2fdcba829b011d951f8d79b56104dfd7c1e2d51
Dinesh <dineshraghavan@ideas2it.com>
2016-03-21 21:26:13 +0530

    Test Commit

--- src/main/java/com/i2i/LoginServlet.java
+++ src/main/java/com/i2i/LoginServlet.java
@@ -25,7 +25,7 @@ public class LoginServlet extends HttpServlet{
 
 	      PrintWriter out = response.getWriter();
 		  String title = "Home";
-		  String content = "hello world github' "+ request.getParameter("username") +" ' your logged in successfully..."; 
+		  String content = "hello world' "+ request.getParameter("username") +" ' your logged in successfully..."; 
 	      String docType =
 	      "<!doctype html public \"-//w3c//dtd html 4.0 " +
 	      "transitional//en\">\n";
