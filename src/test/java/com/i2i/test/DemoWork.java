880c61dac5165bf4c6bee5d6e62c94327bfc4d92
dennyjesu <denny@ideas2it.com>
2016-03-22 18:21:38 +0530

    Updated checklist creation test cases

--- src/test/java/com/i2i/test/DemoWork.java
+++ src/test/java/com/i2i/test/DemoWork.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -31,16 +32,47 @@ public class DemoWork {
     wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Data")));
     WebElement data = driver.findElement(By.linkText("Data"));
     data.click();
+    
     wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='Manage Data']")));
-    WebElement manageData = driver.findElement(By.xpath("//a[text()='Manage Data']"));
-    manageData.click();
+    try {
+		WebElement manageData = driver.findElement(By.xpath("//a[text()='Manage Data']"));
+		manageData.click();
+	} catch (Exception e1) {
+		// TODO Auto-generated catch block
+		System.out.println("Error on clicking ManageData: "+e1);
+	}
+    System.out.println("==================Launching the ManageData form============================");
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Checklist")));
+    try {
+		WebElement checklist = driver.findElement(By.linkText("Checklist"));
+		checklist.click();
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		System.out.println("Error on clicking the checklist: "+e);
+	}
+    System.out.println("==================Launching the checklist form============================");
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.name("item")));
+    WebElement items = driver.findElement(By.name("item"));
+    items.sendKeys("Test data work");
+    WebElement status = driver.findElement(By.name("checkListStatus"));
+    Select mySelect = new Select(status);
+    mySelect.selectByValue("Started");
+    Thread.sleep(2000);
+    WebElement create = driver.findElement(By.id("create"));
+    create.click();
+    System.out.println("==================Created the Checklist============================");
+    
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.className("viewLabel")));
+    String ticket = driver.findElement(By.className("viewLabel")).getText();
+    System.out.println("Created ticket id: "+ticket);
+    
 	}
 	@BeforeMethod
 	public void beforeMethod() {
 
 
 		driver.get("http://192.168.1.173:8080");
-		System.out.println("=========================Launching the Login Page===================================");
+		System.out.println("==================Launching the Login Page============================");
 		driver.manage().window().maximize();
 
 	}
@@ -48,7 +80,7 @@ public class DemoWork {
 	@AfterMethod
 	public void afterMethod() {
 
-		System.out.println("==============================Running Browser close============================");
+		System.out.println("==================Running Browser close============================");
 		//driver.close();
 	}
 
