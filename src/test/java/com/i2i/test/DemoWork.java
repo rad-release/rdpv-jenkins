c5a510c6a9f117e1e5eece1167f25468d6a2ec48
dennyjesu <denny@ideas2it.com>
2016-03-23 11:48:49 +0530

    updated highlight method in test cases

--- src/test/java/com/i2i/test/DemoWork.java
+++ src/test/java/com/i2i/test/DemoWork.java
@@ -1,6 +1,7 @@
 package com.i2i.test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,59 +14,67 @@ import org.testng.annotations.Test;
 
 public class DemoWork {
 
-	WebDriver driver = new FirefoxDriver();
+	static WebDriver driver = new FirefoxDriver();
 	WebDriverWait wait = new WebDriverWait(driver, 40);
 
 	@Test
 	public void validateChecklist() throws InterruptedException {
 
-	
-	wait.until(ExpectedConditions.presenceOfElementLocated(By.id("j_username")));	
-    WebElement user = driver.findElement(By.id("j_username"));
-    user.sendKeys("superuser");
-    WebElement pass = driver.findElement(By.id("password"));
-    pass.sendKeys("superuser");
-    WebElement log = driver.findElement(By.id("btnSubmit"));
-    log.click();
-    System.out.println("==================Launching Home Page============================");
-    Thread.sleep(8000);
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Data")));
-    WebElement data = driver.findElement(By.linkText("Data"));
-    data.click();
-    
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='Manage Data']")));
-    try {
-		WebElement manageData = driver.findElement(By.xpath("//a[text()='Manage Data']"));
-		manageData.click();
-	} catch (Exception e1) {
-		// TODO Auto-generated catch block
-		System.out.println("Error on clicking ManageData: "+e1);
-	}
-    System.out.println("==================Launching the ManageData form============================");
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Checklist")));
-    try {
-		WebElement checklist = driver.findElement(By.linkText("Checklist"));
-		checklist.click();
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		System.out.println("Error on clicking the checklist: "+e);
-	}
-    System.out.println("==================Launching the checklist form============================");
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.name("item")));
-    WebElement items = driver.findElement(By.name("item"));
-    items.sendKeys("Test data work");
-    WebElement status = driver.findElement(By.name("checkListStatus"));
-    Select mySelect = new Select(status);
-    mySelect.selectByValue("Started");
-    Thread.sleep(2000);
-    WebElement create = driver.findElement(By.id("create"));
-    create.click();
-    System.out.println("==================Created the Checklist============================");
-    
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.className("viewLabel")));
-    String ticket = driver.findElement(By.className("viewLabel")).getText();
-    System.out.println("Created ticket id: "+ticket);
-    
+
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("j_username")));	
+		WebElement user = driver.findElement(By.id("j_username"));
+		highlightElement(user);
+		user.sendKeys("superuser");
+		WebElement pass = driver.findElement(By.id("password"));
+		highlightElement(pass);
+		pass.sendKeys("superuser");
+		WebElement log = driver.findElement(By.id("btnSubmit"));
+		highlightElement(log);
+		log.click();
+		System.out.println("==================Launching Home Page============================");
+		Thread.sleep(8000);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Data")));
+		WebElement data = driver.findElement(By.linkText("Data"));
+		highlightElement(data);
+		data.click();
+
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='Manage Data']")));
+		try {
+			WebElement manageData = driver.findElement(By.xpath("//a[text()='Manage Data']"));
+			highlightElement(manageData);
+			manageData.click();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			System.out.println("Error on clicking ManageData: "+e1);
+		}
+		System.out.println("==================Launching the ManageData form============================");
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Checklist")));
+		try {
+			WebElement checklist = driver.findElement(By.linkText("Checklist"));
+			highlightElement(checklist);
+			checklist.click();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error on clicking the checklist: "+e);
+		}
+		System.out.println("==================Launching the checklist form============================");
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.name("item")));
+		WebElement items = driver.findElement(By.name("item"));
+		highlightElement(items);
+		items.sendKeys("Test data work");
+		WebElement status = driver.findElement(By.name("checkListStatus"));
+		Select mySelect = new Select(status);
+		mySelect.selectByValue("Started");
+		Thread.sleep(2000);
+		WebElement create = driver.findElement(By.id("create"));
+		highlightElement(create);
+		create.click();
+		System.out.println("==================Created the Checklist============================");
+
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.className("viewLabel")));
+		String ticket = driver.findElement(By.className("viewLabel")).getText();
+		System.out.println("Created ticket id: "+ticket);
+
 	}
 	@BeforeMethod
 	public void beforeMethod() {
@@ -81,7 +90,19 @@ public class DemoWork {
 	public void afterMethod() {
 
 		System.out.println("==================Running Browser close============================");
-		//driver.close();
+		driver.close();
+	}
+
+
+
+	public static void highlightElement(WebElement element) throws InterruptedException
+	
+	{
+		for (int i=0;i<2;i++)
+		{
+			//((JavascriptExecutor)driver).executeScript("arguments[0].style.backgroundColor='yellow'", element);
+		    ((JavascriptExecutor)driver).executeScript("arguments[0].style.border='3px solid red'", element);
+		}
 	}
 
 }
