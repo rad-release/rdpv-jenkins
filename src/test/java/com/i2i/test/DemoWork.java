e11770f4dbe5129c1b2bcda833c7dce131e19673
dennyjesu <denny@ideas2it.com>
2016-03-29 16:49:43 +0530

    updated the helloworld xml

--- src/test/java/com/i2i/test/DemoWork.java
+++ src/test/java/com/i2i/test/DemoWork.java
@@ -1,10 +1,12 @@
 package com.i2i.test;
 
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,8 +16,9 @@ import org.testng.annotations.Test;
 
 public class DemoWork {
 
-	static WebDriver driver = new FirefoxDriver();
-	WebDriverWait wait = new WebDriverWait(driver, 40);
+
+	WebDriver driver = new FirefoxDriver();
+	WebDriverWait wait = new WebDriverWait(driver, 10);
 
 	@Test
 	public void validateChecklist() throws InterruptedException {
@@ -23,56 +26,52 @@ public class DemoWork {
 
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("j_username")));	
 		WebElement user = driver.findElement(By.id("j_username"));
-		highlightElement(user);
 		user.sendKeys("superuser");
+		System.out.println("userfield");
 		WebElement pass = driver.findElement(By.id("password"));
-		highlightElement(pass);
 		pass.sendKeys("superuser");
 		WebElement log = driver.findElement(By.id("btnSubmit"));
-		highlightElement(log);
 		log.click();
-		System.out.println("==================Launching Home Page============================");
-		Thread.sleep(8000);
+		System.out.println("==================Launching Home Page==============================");
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Data")));
 		WebElement data = driver.findElement(By.linkText("Data"));
-		highlightElement(data);
 		data.click();
-
+		System.out.println("==================Clicking the Data==============================");
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='Manage Data']")));
 		try {
 			WebElement manageData = driver.findElement(By.xpath("//a[text()='Manage Data']"));
-			highlightElement(manageData);
+
 			manageData.click();
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			System.out.println("Error on clicking ManageData: "+e1);
 		}
-		System.out.println("==================Launching the ManageData form============================");
+		System.out.println("==================Launching the ManageData form====================");
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Checklist")));
 		try {
 			WebElement checklist = driver.findElement(By.linkText("Checklist"));
-			highlightElement(checklist);
+
 			checklist.click();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.out.println("Error on clicking the checklist: "+e);
 		}
-		System.out.println("==================Launching the checklist form============================");
+		System.out.println("==================Launching the checklist form=====================");
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.name("item")));
 		WebElement items = driver.findElement(By.name("item"));
-		highlightElement(items);
+
 		items.sendKeys("Test data work");
 		WebElement status = driver.findElement(By.name("checkListStatus"));
 		Select mySelect = new Select(status);
 		mySelect.selectByValue("Started");
 		Thread.sleep(2000);
 		WebElement create = driver.findElement(By.id("create"));
-		highlightElement(create);
+
 		create.click();
 		System.out.println("==================Created the Checklist============================");
-
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.className("viewLabel")));
 		String ticket = driver.findElement(By.className("viewLabel")).getText();
+		System.out.println("==================Getting created checklist Id=====================");
 		System.out.println("Created ticket id: "+ticket);
 
 	}
@@ -81,28 +80,15 @@ public class DemoWork {
 
 
 		driver.get("http://192.168.1.173:8080");
-		System.out.println("==================Launching the Login Page============================");
+		System.out.println("==================Launching the Login Page=========================");
 		driver.manage().window().maximize();
 
 	}
 
 	@AfterMethod
 	public void afterMethod() {
-
 		System.out.println("==================Running Browser close============================");
 		driver.close();
-	}
-
-
-
-	public static void highlightElement(WebElement element) throws InterruptedException
-	
-	{
-		for (int i=0;i<2;i++)
-		{
-			//((JavascriptExecutor)driver).executeScript("arguments[0].style.backgroundColor='yellow'", element);
-		    ((JavascriptExecutor)driver).executeScript("arguments[0].style.border='3px solid red'", element);
-		}
 	}
 
 }
