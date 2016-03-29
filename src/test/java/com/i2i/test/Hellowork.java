ff7d32dca08a1519ea49c0f8ff2febfa9306368a
Dinesh <dineshraghavan@ideas2it.com>
2016-03-29 19:25:24 +0530

    HelloWork changes

--- src/test/java/com/i2i/test/Hellowork.java
+++ src/test/java/com/i2i/test/Hellowork.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import junit.framework.Assert;
 
 public class Hellowork {
 
