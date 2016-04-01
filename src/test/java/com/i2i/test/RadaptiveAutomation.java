dbe33c6b953c8539dfdc458b740c0bf202491fcb
Dinesh <dineshraghavan@ideas2it.com>
2016-04-01 14:54:33 +0530

    Class imported

--- src/test/java/com/i2i/test/RadaptiveAutomation.java
+++ src/test/java/com/i2i/test/RadaptiveAutomation.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 public class RadaptiveAutomation {
 
