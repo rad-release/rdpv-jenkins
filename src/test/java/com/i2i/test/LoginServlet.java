4ad50cda86c6330c0c1bb82de08bbcb968c3bb1b
dennyjesu <denny@ideas2it.com>
2016-06-15 19:41:48 +0530

    Updated the testcases

---
+++ src/test/java/com/i2i/test/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.i2i.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// Method to handle GET method request.
+	  public void doGet(HttpServletRequest request,
+	                    HttpServletResponse response)
+	            throws ServletException, IOException
+	  {
+	      // Set response content type
+	      response.setContentType("text/html");
+
+	      PrintWriter out = response.getWriter();
+		  String title = "Home";
+		  String content = "hello' "+ request.getParameter("username") +" ' your logged in successfully..."; 
+	      String docType =
+	      "<!doctype html public \"-//w3c//dtd html 4.0 " +
+	      "transitional//en\">\n";
+	      out.println(docType +
+	                "<html>\n" +
+	                "<head><title>" + title + "</title></head>\n" +
+	                "<body bgcolor=\"#f0f0f0\">\n" +
+	                "<h1 align=\"center\">" + content + "</h1>\n" +
+	                "</body></html>");
+	  }
+	  // Method to handle POST method request.
+	  public void doPost(HttpServletRequest request,
+	                     HttpServletResponse response)
+	      throws ServletException, IOException {
+	     doGet(request, response);
+	  }
+}
